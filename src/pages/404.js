223068f1730b15bb0474a7c00f2e67a807afd449
Meehoi <beam_baimon@hotmail.com>
2020-07-05 23:39:10 +0700

    fix typo in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
   <div className={styles.notFound}>
     <SEO title="404" />
     <h2>404</h2>
-    <p>We cannot find the page you looking for.</p>
+    <p>We cannot find the page you're looking for.</p>
   </div>
 )
 
